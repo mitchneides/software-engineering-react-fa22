67949983bc10b0326a6c050aace9a608c367773d
mitchneides <mitch.neides@gmail.com>
2022-11-04 17:38:17 -0400

    completes create tuit test

--- src/tests/tuits-service.test.js
+++ src/tests/tuits-service.test.js
@@ -3,39 +3,65 @@ import {
   deleteTuit, findTuitById,
   findAllTuits
 } from "../services/tuits-service";
+import {
+    deleteUsersByUsername, createUser
+} from "../services/users-service";
 
-// ******************************** breaks node see errors ********************
-describe('deleteTuit', () => {
+
+describe.only('deleteTuit', () => {
     // sample tuit to insert
-    const sample = {
+    const sampleTuit = {
+        _id: '111111111111111111111111',
         tuit: "Alice's test tuit",
-        postedBy: '633c41de89045f21193ea004'
+        postedOn: '2022-11-04T00:00:00.000Z'
+    };
+
+  // sample user to author the tuit
+    const ripley = {
+        username: 'ellenripley',
+        password: 'lv426',
+        email: 'ellenripley@aliens.com'
     };
 
     // setup before running
-    // ******************************* cannot get id like this *********************
     beforeAll(() => {
+        // tracks promises to fulfill before returning
+        let promises = [];
         // remove any/all tuits to make sure we create it in the test
-        return deleteTuit(sample._id);
+        promises.push(deleteTuit(sampleTuit._id));
+        // remove user
+        promises.push(deleteUsersByUsername(ripley.username));
+        // Return once all promises fulfilled
+        return Promise.all(promises)
     })
 
     // clean up after test runs
-    // ******************************* cannot get id like this *********************
     afterAll (() => {
-        return deleteTuit(sample._id);
+        // tracks promises to fulfill before returning
+        let promises = [];
+        // remove any/all tuits to make sure we create it in the test
+        promises.push(deleteTuit(sampleTuit._id));
+        // remove user
+        promises.push(deleteUsersByUsername(ripley.username));
+        // Return once all promises fulfilled
+        return Promise.all(promises)
     })
 
+    // run test
     test('can create a new tuit with REST API', async () => {
+        // create user to author tuit
+        const newUser = await createUser(ripley);
         // insert new tuit in the database
-        const newTuit = await createTuit(sample);
+        const newTuit = await createTuit(newUser._id, sampleTuit);
 
         // verify inserted tuit's properties
-        expect(newTuit.tuit).toEqual(sample.tuit);
-        expect(newTuit.postedBy).toString().toEqual(sample.postedBy);
+        expect(newTuit.tuit).toEqual(newTuit.tuit);
+        expect(newTuit.postedBy).toEqual(newUser._id);
+        expect(newTuit.postedOn).toEqual(newTuit.postedOn);
     });
 });
 
-describe('can delete tuit wtih REST API', () => {
+describe('can delete tuit with REST API', () => {
   // TODO: implement this
 });
 
