00662caf65c70ea70b5ec3f14dde0a9755534cec
mitchneides <mitch.neides@gmail.com>
2022-12-13 17:15:06 -0500

    adds dislikes profile components

--- src/components/profile/index.js
+++ src/components/profile/index.js
@@ -3,6 +3,7 @@ import {Route, Routes, useNavigate, useLocation, Link} from "react-router-dom";
 import {useEffect, useState} from "react";
 import MyTuits from "./my-tuits";
 import MyLikes from "./my-likes"
+import MyDislikes from "./my-dislikes";
 
 const Profile = () => {
   const location = useLocation();
@@ -35,10 +36,21 @@ const Profile = () => {
                         className={`nav-link ${location.pathname.indexOf('mytuits') >= 0 ? 'active': ''}`}>
                       Tuits</Link>
               </li>
+              <li className="nav-item">
+                  <Link to="/profile/mylikes"
+                        className={`nav-link ${location.pathname.indexOf('mylikes') >= 0 ? 'active': ''}`}>
+                      Likes</Link>
+              </li>
+              <li className="nav-item">
+                  <Link to="/profile/mydislikes"
+                        className={`nav-link ${location.pathname.indexOf('mydislikes') >= 0 ? 'active': ''}`}>
+                      Dislikes</Link>
+              </li>
           </ul>
           <Routes>
-              <Route path="/mytuits"
-                     element={<MyTuits/>}/>
+              <Route path="/mytuits" element={<MyTuits/>}/>
+              <Route path="/mylikes" element={<MyLikes/>}/>
+              <Route path="/mydislikes" element={<MyDislikes/>}/>
           </Routes>
       </div>
   );
